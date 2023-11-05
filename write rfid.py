df6c7649dc72ad7765918f4deb23f1cd52604546
Sarthak Rajendra Rohekar <105600931+sarthakrohekar@users.noreply.github.com>
2023-11-06 05:27:22 +0530

    Create write rfid.py

---
+++ write rfid.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+
+from mfrc522 import SimpleMFRC522
+
+reader = SimpleMFRC522()
+
+try:
+
+  text = input('New data:’)
+
+  print("Now place your tag to write")
+
+  reader.write(text)
+
+  print("Written")
+
+finally:
+
+GPIO.cleanup()
